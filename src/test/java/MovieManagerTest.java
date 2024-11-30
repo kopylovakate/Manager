9f4fd4c5817f917196c39e4a292dfbca5419b37a
katana <klimenko.katrin888@mail.ru>
2024-11-30 19:18:33 +0300

    one

--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
 
 public class MovieManagerTest {
+
+    @Test
+    public void test() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film I");
+        manager.addMovie("Film II");
+        manager.addMovie("Film III");
+
+        String[] expected = { "Film I", "Film II", "Film III" };
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test2
+    public void test() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film I");
+        manager.addMovie("Film II");
+        manager.addMovie("Film III");
+
+        String[] expected = { "Film III", "Film II", "Film I" };
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
 }
